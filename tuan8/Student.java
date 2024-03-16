dbbf197cae65e41f66a95b365ae2ec0ea1465ab5
neyugn1901 <nguyen123123k@gmail.com>
2024-03-16 14:41:33 +0700

    a

---
+++ tuan8/Student.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.baitap.TUAN8;
+import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ *
+ * @author nguye
+ */
+public class Student implements Serializable{
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Student() {
+        
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
